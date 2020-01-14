a88f58cf79b46219c0c93ab059b210143e61a5e0
TheJeronator <45825757+TheJeronator@users.noreply.github.com>
2020-01-15 00:32:18 +0100

    Added mountain

--- MoveTransformRB/MoveTransformRB/Assets/Scripts/VRLocomotion.cs
+++ MoveTransformRB/MoveTransformRB/Assets/Scripts/VRLocomotion.cs
@@ -5,82 +5,99 @@ using Valve.VR;
 
 public class VRLocomotion : MonoBehaviour
 {
-    public float Sens = 0.1f;
-    public float maxSpeed = 1.0f;
-    //
-    public SteamVR_Action_Boolean movePress = null;
-    public SteamVR_Action_Vector2 moveValue = null;
-    // Private 
-    private float speed = 0.0f;
-    private CharacterController charControl = null;
-    private Transform cameraRig = null;
-    private Transform head = null;
+    public float m_Gravity = 30.0f;
+    public float m_Sensitivity = 0.1f;
+    public float m_MaxSpeed = 1.0f;
+    public float m_RotateIncrement = 90;
+
+    public SteamVR_Action_Boolean m_RotatePress = null;
+    public SteamVR_Action_Boolean m_MovePress = null;
+    public SteamVR_Action_Vector2 m_MoveValue = null;
+
+    private float m_Speed = 0.0f;
+
+    private CharacterController m_CharacterController = null;
+    private Transform m_CameraRig = null;
+    private Transform m_Head = null;
 
     private void Awake()
     {
-        charControl = GetComponent<CharacterController>();
+        m_CharacterController = GetComponent<CharacterController>();
     }
 
-
+    // Start is called before the first frame update
     private void Start()
     {
-        cameraRig = SteamVR_Render.Top().origin;
-        head = SteamVR_Render.Top().head;
+        m_CameraRig = SteamVR_Render.Top().origin;
+        m_Head = SteamVR_Render.Top().head;
     }
 
-
+    // Update is called once per frame
     private void Update()
     {
-        handleHead();
-        calcMovement();
-        handleHeight();
+        HandleHeight();
+        CalculateMovement();
+        SnapRotation();
     }
 
-    private void handleHead()
+    private void HandleHeight()
     {
-        Vector3 oldPos = cameraRig.position;
-        Quaternion oldRotation = cameraRig.rotation;
+        // Get the head in local space
+        float headHeight = Mathf.Clamp(m_Head.localPosition.y, 1, 2);
+        m_CharacterController.height = headHeight;
 
-        transform.eulerAngles = new Vector3(0.0f, head.rotation.eulerAngles.y, 0.0f);
+        // Cut in half
+        Vector3 newCenter = Vector3.zero;
+        newCenter.y = m_CharacterController.height / 2;
+        newCenter.y += m_CharacterController.skinWidth;
 
-        cameraRig.position = oldPos;
-        cameraRig.rotation = oldRotation;
+        // Move capsule in local space
+        newCenter.x = m_Head.localPosition.x;
+        newCenter.z = m_Head.localPosition.z;
+
+        // Apply
+        m_CharacterController.center = newCenter;
     }
-
-    private void calcMovement()
+    private void CalculateMovement()
     {
-        Vector3 orientationEuler = new Vector3(0, transform.eulerAngles.y, 0);
-        Quaternion orientation = Quaternion.Euler(orientationEuler);
+        Quaternion orientation = CalculateOrientation();
+        // Figure out movement orientation
         Vector3 movement = Vector3.zero;
 
-        if (movePress.GetStateUp(SteamVR_Input_Sources.Any))
-            speed = 0;
+        // If not moving
+        if (m_MoveValue.axis.magnitude == 0)
+            m_Speed = 0;
 
-        if (movePress.state)
-        {
-            speed += moveValue.axis.y * Sens;
-            speed = Mathf.Clamp(speed, -maxSpeed, maxSpeed);
+        // Add, clamp
+        m_Speed += m_MoveValue.axis.magnitude * m_Sensitivity;
+        m_Speed = Mathf.Clamp(m_Speed, -m_MaxSpeed * 0.5f, m_MaxSpeed);
 
-            movement += orientation * (speed * Vector3.forward) * Time.deltaTime;
-            Debug.Log(movement);
-            Debug.Log(speed);
-        }
-        charControl.Move(movement);
-        
+        // Orientation and gravity
+        movement += orientation * (m_Speed * Vector3.forward);
+        movement.y -= m_Gravity * Time.deltaTime;
+
+        // Apply
+        m_CharacterController.Move(movement * Time.deltaTime);
     }
-    private void handleHeight()
+
+    private Quaternion CalculateOrientation()
     {
-        float headHeight = Mathf.Clamp(head.localPosition.y, 1, 2);
-        charControl.height = headHeight;
+        float rotation = Mathf.Atan2(m_MoveValue.axis.x, m_MoveValue.axis.y);
+        rotation *= Mathf.Rad2Deg;
 
-        Vector3 newCenter = Vector3.zero;
-        newCenter.y = charControl.height / 2;
-        newCenter.y += charControl.skinWidth;
-        newCenter.x += head.localPosition.x;
-        newCenter.z += head.localPosition.z;
+        Vector3 orientationEuler = new Vector3(0, m_Head.eulerAngles.y + rotation, 0);
+        return Quaternion.Euler(orientationEuler);
+    }
+    private void SnapRotation()
+    {
+        float snapValue = 0.0f;
 
-        newCenter = Quaternion.Euler(0, -transform.eulerAngles.y, 0) * newCenter;
+        if (m_RotatePress.GetStateDown(SteamVR_Input_Sources.LeftHand))
+            snapValue = -Mathf.Abs(m_RotateIncrement);
 
-        charControl.center = newCenter;
+        if (m_RotatePress.GetStateDown(SteamVR_Input_Sources.RightHand))
+            snapValue = Mathf.Abs(m_RotateIncrement);
+
+        transform.RotateAround(m_Head.position, Vector3.up, snapValue);
     }
 }
