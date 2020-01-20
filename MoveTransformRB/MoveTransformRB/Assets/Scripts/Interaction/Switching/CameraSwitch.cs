9f88cce62e3814400100c7f53c2464b850ebc5d6
TheJeronator <45825757+TheJeronator@users.noreply.github.com>
2020-01-20 02:26:29 +0100

    Working polaroid switch + stairs
    Stairs implemented
    Player can switch between controller and camera
    First bridge also implemented

---
+++ MoveTransformRB/MoveTransformRB/Assets/Scripts/Interaction/Switching/CameraSwitch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class CameraSwitch : MonoBehaviour
+{
+    public MeshRenderer Mesh = null;
+    public GameObject controller = null;
+
+    public void Awake()
+    {
+        Mesh = GetComponentInChildren<MeshRenderer>();
+    }
+    public void CameraSwitching()
+    {
+        if (Mesh.enabled == false)
+        {
+            Mesh.enabled = true;
+            controller.SetActive(false);
+        }
+        else
+        {
+            Mesh.enabled = false;
+            controller.SetActive(true);
+        }
+    }
+}
--- MoveTransformRB/MoveTransformRB/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ MoveTransformRB/MoveTransformRB/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -113,6 +113,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Boolean CameraSwap
+        {
+            get
+            {
+                return SteamVR_Actions.default_CameraSwap;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
